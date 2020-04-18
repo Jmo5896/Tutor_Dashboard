9756023158eebbb4567788e89b1afd188d8fce42
jmo5896 <justin.m.moore5896@gmail.com>
2020-04-18 15:31:42 -0500

    just added barchart of all your current students and how many sessions each has had in total

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,62 @@
 console.log('js is loaded');
-let myData;
+let sessionData;
+let currentStudents;
 
 const googleSheetURL1 = `https://spreadsheets.google.com/feeds/cells/${WORKBOOK}/1/public/full?alt=json`;
 const googleSheetURL2 = `https://spreadsheets.google.com/feeds/cells/${WORKBOOK}/2/public/full?alt=json`;
 d3.json(googleSheetURL1).then(data => {
-    myData= cleanSheets(data);
-    console.log(myData);
+    sessionData = cleanSheets(data);
+    console.log(sessionData);
+    d3.json(googleSheetURL2).then(data => {
+        currentStudents = cleanSheets(data);
+        console.log(currentStudents);
+        currentStudentAnalysis(currentStudents);
+    });
 });
 
-d3.json(googleSheetURL2).then(data => {
-    console.log(data)
-    myData= cleanSheets(data);
-    console.log(myData);
-});
+
+function unpack(data, key) {
+    return data.map(obj => obj[key]);
+}
+
+function currentStudentAnalysis(data) {
+    const names = unpack(data, 'Student Name');
+    const studentTallies = names.map(name => {
+        return sessionData.filter(obj => obj['Student Name'] == name).length;
+    });
+
+    const currentTrace = {
+        x: names,
+        y: studentTallies,
+        type: 'bar',
+        text: studentTallies.map(String),
+        textposition: 'auto',
+        hoverinfo: 'none',
+        marker: {
+            color: 'rgb(158,202,225)',
+            opacity: 0.6,
+            line: {
+                color: 'rgb(8,48,107)',
+                width: 1.5
+            }
+        }
+    };
+
+    var currentStudentData = [currentTrace];
+
+    var currentStudentlayout = {
+        title: 'January 2013 Sales Report',
+        barmode: 'stack',
+        height: 500,
+        margin: {
+            l: 20,
+            r: 0,
+          },
+    };
+
+    Plotly.newPlot('curentStudents', currentStudentData, currentStudentlayout);
+}
 
 //charts that I want
-    //anlysis on students
+    //anlysis on students with the most sessions of all time
+    //analysis on CURRENT students
